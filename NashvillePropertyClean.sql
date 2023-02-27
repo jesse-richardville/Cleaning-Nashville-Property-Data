877c5f607f4a05b256bcf6ecb12442c4e90bd7b7
Jesse Richardville <115645317+jesse-richardville@users.noreply.github.com>
2023-02-26 21:14:01 -0600

    Add files via upload

---
+++ NashvillePropertyClean.sql
@@ -0,0 +1,313 @@
+/*
+
+SQL Data Cleaning
+
+*/
+
+SELECT *
+FROM Projects.dbo.NashvilleHousing
+
+
+-- Configure SaleDate
+
+SELECT SaleDate
+FROM Projects.dbo.NashvilleHousing
+
+SELECT 
+  SaleDate, 
+  CONVERT(Date, SaleDate) 
+
+-- If it won't Update
+FROM 
+  Projects.dbo.NashvilleHousing 
+ALTER TABLE 
+  NashvilleHousing 
+ADD 
+  SaleDateUpdate Date;
+UPDATE 
+  NashvilleHousing 
+SET 
+  SaleDateUpdate = CONVERT(Date, SaleDate)
+
+--check
+SELECT 
+  SaleDateUpdate, 
+  CONVERT(Date, SaleDate) 
+FROM 
+  Projects.dbo.NashvilleHousing 
+
+
+-- Populate (NULL) PropertyAddress data based on ParcelID, update table changes
+
+SELECT * 
+FROM 
+  Projects.dbo.NashvilleHousing --WHERE PropertyAddress is NULL
+ORDER BY 
+  ParcelID 
+SELECT 
+  a.ParcelID, 
+  a.PropertyAddress, 
+  b.ParcelID, 
+  b.PropertyAddress, 
+  ISNULL(
+    a.PropertyAddress, b.PropertyAddress
+  ) 
+FROM 
+  Projects.dbo.NashvilleHousing AS a 
+  JOIN Projects.dbo.NashvilleHousing AS b ON a.ParcelID = b.ParcelID 
+  AND a.[UniqueID ] <> b.[UniqueID ] 
+WHERE 
+  a.PropertyAddress is NULL 
+UPDATE a 
+SET 
+  PropertyAddress = ISNULL(
+    a.PropertyAddress, b.PropertyAddress
+  ) 
+FROM 
+  Projects.dbo.NashvilleHousing AS a 
+  JOIN Projects.dbo.NashvilleHousing AS b ON a.ParcelID = b.ParcelID 
+  AND a.[UniqueID ] <> b.[UniqueID ] 
+WHERE 
+  a.PropertyAddress is NULL
+
+
+-- Split PropertyAddress/OwnerAddress into separate columns (Address, City, State)
+
+SELECT 
+  PropertyAddress 
+FROM 
+  Projects.dbo.NashvilleHousing 
+SELECT 
+  SUBSTRING(
+    PropertyAddress, 
+    1, 
+    CHARINDEX(',', PropertyAddress) -1
+  ) AS Address, 
+  SUBSTRING(
+    PropertyAddress, 
+    CHARINDEX(',', PropertyAddress) + 1, 
+    LEN(PropertyAddress)
+  ) AS City 
+FROM 
+  Projects.dbo.NashvilleHousing 
+ALTER TABLE 
+  NashvilleHousing 
+ADD 
+  PropertySplitAddress NVARCHAR(255);
+UPDATE 
+  NashvilleHousing 
+SET 
+  PropertySplitAddress = SUBSTRING(
+    PropertyAddress, 
+    1, 
+    CHARINDEX(',', PropertyAddress) -1
+  ) 
+ALTER TABLE 
+  NashvilleHousing 
+ADD 
+  PropertySplitCity NVARCHAR(255);
+UPDATE 
+  NashvilleHousing 
+SET 
+  PropertySplitCity = SUBSTRING(
+    PropertyAddress, 
+    CHARINDEX(',', PropertyAddress) + 1, 
+    LEN(PropertyAddress)
+  ) 
+
+--check columns to the right of table for updates
+SELECT * 
+FROM 
+  Projects.dbo.NashvilleHousing 
+SELECT 
+  OwnerAddress 
+FROM 
+  Projects.dbo.NashvilleHousing 
+SELECT 
+  PARSENAME(
+    REPLACE(OwnerAddress, ',', '.'), 
+    3
+  ) AS OwnerSplitAddress, 
+  PARSENAME(
+    REPLACE(OwnerAddress, ',', '.'), 
+    2
+  ) AS OwnerSplitCity, 
+  PARSENAME(
+    REPLACE(OwnerAddress, ',', '.'), 
+    1
+  ) AS OwnerSplitState 
+FROM 
+  Projects.dbo.NashvilleHousing 
+
+--Run first before update (below)
+ALTER TABLE 
+  NashvilleHousing 
+ADD 
+  OwnerSplitAddress NVARCHAR(255); 
+
+UPDATE 
+  NashvilleHousing 
+SET 
+  OwnerSplitAddress = PARSENAME(
+    REPLACE(OwnerAddress, ',', '.'), 
+    3
+  ) --Adds column to the table
+
+--Run first before update (below)
+ALTER TABLE 
+  NashvilleHousing 
+ADD 
+  OwnerSplitCity NVARCHAR(255); 
+
+UPDATE 
+  NashvilleHousing 
+SET 
+  OwnerSplitCity = PARSENAME(
+    REPLACE(OwnerAddress, ',', '.'), 
+    2
+  ) --Adds column to the table
+  
+--Run first before update (below)
+ALTER TABLE 
+  NashvilleHousing 
+ADD 
+  OwnerSplitState NVARCHAR(255);
+
+UPDATE 
+  NashvilleHousing 
+SET 
+  OwnerSplitState = PARSENAME(
+    REPLACE(OwnerAddress, ',', '.'), 
+    1
+  ) --Adds column to the table
+
+--check columns for updates/new columns
+SELECT * 
+FROM 
+  Projects.dbo.NashvilleHousing
+
+
+-- Normalize SoldAsVacant values to Yes/No
+
+SELECT 
+  DISTINCT(SoldAsVacant), 
+  COUNT(SoldAsVacant) 
+FROM 
+  Projects.dbo.NashvilleHousing 
+GROUP BY 
+  SoldAsVacant 
+ORDER BY 
+  2 --check disparities in SoldAsVacant column
+
+SELECT 
+  SoldAsVacant, 
+  CASE WHEN SoldAsVacant = 'Y' THEN 'Yes' WHEN SoldAsVacant = 'N' THEN 'NO' ELSE SoldAsVacant END 
+FROM 
+  Projects.dbo.NashvilleHousing 
+
+SELECT 
+  SoldAsVacant = 'N' 
+FROM 
+  Projects.dbo.NashvilleHousing 
+UPDATE 
+  NashvilleHousing 
+SET 
+  SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes' 
+  WHEN SoldAsVacant = 'N' THEN 'NO' 
+  ELSE SoldAsVacant 
+  END 
+  
+--rerun to check SoldAsVacant column for Y/N
+SELECT 
+  DISTINCT(SoldAsVacant), 
+  COUNT(SoldAsVacant) 
+FROM 
+  Projects.dbo.NashvilleHousing 
+GROUP BY 
+  SoldAsVacant 
+ORDER BY 
+  2
+
+
+-- Remove duplicates with CTE
+
+WITH RowNumCTE AS(
+  SELECT 
+    *, 
+    ROW_NUMBER() OVER (
+      PARTITION BY ParcelID, 
+      PropertyAddress, 
+      SalePrice, 
+      SaleDate, 
+      LegalReference 
+      ORDER BY 
+        UniqueID
+    ) AS row_num 
+  FROM 
+    Projects.dbo.NashvilleHousing 
+	--ORDER BY [UniqueID ]
+    ) 
+
+SELECT * 
+FROM 
+  RowNumCTE 
+WHERE 
+  row_num > 1 
+ORDER BY 
+  PropertyAddress --check for duplicates (104 found)
+  
+  WITH RowNumCTE AS(
+    SELECT *, 
+      ROW_NUMBER() OVER (
+        PARTITION BY ParcelID, 
+        PropertyAddress, 
+        SalePrice, 
+        SaleDate, 
+        LegalReference 
+        ORDER BY 
+          UniqueID
+      ) AS row_num 
+    FROM 
+      Projects.dbo.NashvilleHousing 
+      --ORDER BY [UniqueID ]
+      ) 
+DELETE FROM 
+  RowNumCTE 
+WHERE 
+  row_num > 1 --remove duplicates 
+  
+--recheck for duplicates
+  WITH RowNumCTE AS(
+    SELECT *, 
+      ROW_NUMBER() OVER (
+        PARTITION BY ParcelID, 
+            PropertyAddress, 
+            SalePrice, 
+            SaleDate, 
+            LegalReference 
+            ORDER BY 
+            UniqueID
+            ) AS row_num 
+    FROM 
+      Projects.dbo.NashvilleHousing 
+	  --ORDER BY [UniqueID ]
+      ) 
+SELECT * 
+FROM 
+  RowNumCTE 
+WHERE 
+  row_num > 1
+
+
+-- Delete unused columns
+
+SELECT *
+FROM
+  Projects.dbo.NashvilleHousing
+
+  ALTER TABLE
+    Projects.dbo.NashvilleHousing
+  DROP COLUMN PropertyAddress, SaleDate, OwnerAddress
+  
+
+
---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+# SQL Data Cleaning, Nashville Property 
+## ReadMe
+### Skills used: MS SQL Server (Convert, Populate Nulls, Split, CTE, Remove Duplicates, Remove Columns, Case, Aggregate) and Power BI 
+
+## Description
+
+This is a data cleaning project using SQL to clean a Nashville property dataset in SSMS to then analyze results using Power BI for visuals.
+
+Raw data was imported and can be viewed in the "RAW_%" file.
+
+Data was queried using SSMS and has been documented in the ".sql" file.
+
+The data was cleaned and transformed using SSMS to create useful queries for the project and has been extracted and saved as a ".csv" file, which was imported into Microsoft Power BI for visualization.
+
+The Power BI dashboard can be viewed [here](https://app.powerbi.com/reportEmbed?reportId=ba83fa40-10c0-4e3f-afd3-dc4497417b83&autoAuth=true&ctid=dd9514d9-0843-4d4f-8158-06bf09d7ea7c).
+https://app.powerbi.com/links/qmpaDv7X2J?ctid=dd9514d9-0843-4d4f-8158-06bf09d7ea7c&pbi_source=linkShare
+
+## Summary
+
+Cleaned Nashville Property data using SQL and Power BI to analyze the distribution of property types, value, plot size, and sale price between January 2, 2013 - September 13, 2016 in the Nashville, TN region. Analysis leads to the conclusion that during this time frame, $6.32 billion in sales of 26 property types were made in 10 cities with a median sale price of $184,450 in the Nashville area. Single family homes were the most popular property type sold (91.7% of all sales with a median value of $191,500), 3 bedroom properties were sold the most (43.91% of all sales), late spring and summer months (11.45% in May, 13.89% in June, 10.66% in July, and 10.43% in August of all monthly sales) had the highest sales by month throughout the year, with spikes on days in the middle of the month (15th day) and beginning/end of the month (on the 1st and 30th/31st). Brentwood is the most expensive city in the region with a median property sale price of $420,000.
